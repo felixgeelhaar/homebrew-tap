3407408bf892123d11714da540e64337fd2ad21f
Felix Geelhaar <felix@felixgeelhaar.de>
2026-01-01 13:36:17 +0100

    Update preflight formula to v4.3.0

--- Formula/preflight.rb
+++ Formula/preflight.rb
@@ -3,7 +3,7 @@
 class Preflight < Formula
   desc "Deterministic workstation compiler"
   homepage "https://github.com/felixgeelhaar/preflight"
-  version "4.2.0"
+  version "4.3.0"
   license "MIT"
 
   on_macos do
